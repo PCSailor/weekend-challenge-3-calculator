55a514f4b23d2c4d4b946167d17f536ccc98b399
Phil Curtis <Phil@PhilCurtis.io>
2017-02-24 17:09:17 -0600

    initial commit

---
+++ server/app.js
@@ -0,0 +1,4 @@
+// and the Server code would have something like
+// router.post('/division', function(req, res){
+//   //some code here
+// });
---
+++ server/public/client.js
@@ -0,0 +1,8 @@
+Client code might look like this:
+$.ajax({
+    type: "POST",
+    url: "/division",
+    success: function(data){
+       //something here
+    }
+});
