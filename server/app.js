db1db0816e8fa7c5b3e910f82c3eb23915e7434d
Phil Curtis <Phil@PhilCurtis.io>
2017-02-25 13:43:39 -0600

    ajax/app-POST added, not communicating but all working

--- server/app.js
+++ server/app.js
@@ -2,3 +2,43 @@
 // router.post('/division', function(req, res){
 //   //some code here
 // });
+console.log('app.js success & mock server listening on Port 5000');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use (express.static('server/public'));
+app.use (bodyParser.urlencoded({extended: true}));
+
+
+app.post('/calc', function(req, res) {
+  var newCalc = req.body;
+  console.log('new calc: ', newCalc);
+  res.sendStatus(200);
+});
+
+
+
+
+// NOTE: From Vanilla Node/app.js
+// var numberOne = 7;
+// var numberTwo = 4;
+// // NOTE: Multiplication
+// console.log(calculator.multiply(numberOne, numberTwo));
+// function multiply (firstNum, secondNum) {
+//   var product = firstNum * secondNum;
+//   return product;
+// }
+// // NOTE: Server
+// var crazy = calculator.crazy(numberOne);
+// var http = require('http'); // http built into node code as object
+//
+// http.createServer(function(req, res){
+//   res.writeHead(200);
+//   res.write('crazy'); // write status
+//   res.end();
+// }).listen(5000); //doesn't need to be 5000
+// NOTE: END From Vanilla Node/app.js
+
+
+app.listen(5000);
+console.log('Listening to Port 5000');
--- server/public/client.js
+++ server/public/client.js
@@ -1,8 +1,36 @@
-Client code might look like this:
-$.ajax({
-    type: "POST",
-    url: "/division",
-    success: function(data){
-       //something here
-    }
+// Client code might look like this:
+// $.ajax({
+//     type: "POST",
+//     url: "/division",
+//     success: function(data){
+//        //something here
+//     }
+// });
+$(document).ready(function () {
+console.log('jQuery is sourced');
+
+$('#multiply').on('click', function(){
+  console.log('multiply has been clicked');
 });
+
+$('#result').on('click', function(num1 , num2 ){
+  var num1 = $('#num1').val()
+  var num2 = $('#num2').val()
+  console.log(num1 * num2);
+  console.log(' times the result has been clicked');
+});
+
+
+$.ajax({
+type: 'POST',
+url: '/',
+data: calaData,
+success: function(response) {
+  console.log('response from server is a success!');
+}
+});
+
+
+
+}); // NOTE: FOR: $(document).ready(function () {
+console.log('Javascript sourced');
