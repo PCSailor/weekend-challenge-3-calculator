14b13c6b87711b19a84617fd883f1f01f51336d1
Phil Curtis <Phil@PhilCurtis.io>
2017-02-26 11:14:56 -0600

    html/DOM/server talking with response back

--- server/app.js
+++ server/app.js
@@ -6,17 +6,35 @@ console.log('app.js success & mock server listening on Port 5000');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-app.use (express.static('server/public'));
-app.use (bodyParser.urlencoded({extended: true}));
+app.use (express.static('server/public')); // NOTE: Is it a static file?
+app.use (bodyParser.urlencoded({extended: true})); // NOTE: does the request have data? Yes? Creates req.body & holds it until called upon  within app.post
+var calculations = [];
 
 
-app.post('/calc', function(req, res) {
-  var newCalc = req.body;
-  console.log('new calc: ', newCalc);
-  res.sendStatus(200);
+var calcOne = "";
+var calcTwo = "";
+app.post('/', function(req, res) {
+  var newCalc = req.body; // NOTE: .data from client.js becomes this req.body.  Now have the object from client.js within this variable
+  calculations.push(newCalc);
+  console.log('var newCalc = req.body; returns: ', newCalc);
+  console.log(typeof newCalc);
+  var calcOne = newCalc.$num1;
+  var calcTwo = newCalc.$num2;
+  var result = (calcOne - calcTwo).toString();
+  // var stringResult = result.toString();
+  console.log(result);
+  console.log(typeof result);
+  res.send(result.toString());
+  console.log(result);
+  // res.sendStatus(200);
+
 });
+// console.log(calcOne, calcTwo);
+// app.get('/', function(req, res) {
+//   res.send(200);
+// });
 
-
+// NOTE: use res.send('whatever you're trying to send back');
 
 
 // NOTE: From Vanilla Node/app.js
--- server/public/client.js
+++ server/public/client.js
@@ -13,24 +13,55 @@ $('#multiply').on('click', function(){
   console.log('multiply has been clicked');
 });
 
+////////////////////////////////////////////////// NOTE: Into Server ////////////////////////////////////////////////
+////////////////////////////////////////////////// NOTE: Into Server ////////////////////////////////////////////////
+////////////////////////////////////////////////// NOTE: Into Server ////////////////////////////////////////////////
+// On click I want to run a function that takes the two entered numbers and the PEMDAS within the function, console logged, and then passes these values over to the server
+  // var numArray = {$num1, $num2};
+  // var $num1 = $('#num1').val();
+  // var $num2 = $('#num2').val();
+
+// $.ajax({
+//   type: 'GET',
+//   url: '/',
+//   success: function(response){
+//     console.log('Ajax-GET Response: ', response);
+//     // $('numArray').empty();  // NOTE: Empty the object
+//   }
+// })
+
 $('#result').on('click', function(num1 , num2 ){
-  var num1 = $('#num1').val()
-  var num2 = $('#num2').val()
-  console.log(num1 * num2);
-  console.log(' times the result has been clicked');
-});
+  console.log('Result Button clicked');
+  var $num1 = $('#num1').val();  // NOTE: moved to global
+  var $num2 = $('#num2').val();  // NOTE: moved to global
+  var numArray = {$num1, $num2};  // NOTE: moved to global
+  console.log(numArray);
+  // function ajaxPost() {
+    $.ajax({
+      type: 'POST', // NOTE: These two lines are retrieving AND posting data
+      url: '/',
+      data: numArray, // NOTE: data becomes req.body on the server side
+      success: function(response) {
+        console.log('Successful response from server back here to client.  Result is: ', response);
+        console.log(response);
+      }
+    });// NOTE: FOR: $.ajax({
+    // }
+  });// NOTE: FOR: $('#result').on('click', function(num1 , num2 ){
 
+      // $('#calculation').append('numbers-here');
+////////////////////////////////////////////////// NOTE: Failed Code ////////////////////////////////////////////////
+// console.log('num1 = ', $num1);
+// console.log('num2 = ', $num2);
+// var $result = $num1 * $num2; // Error:
+// console.log('$result = ', $result);
+// console.log('num1 * num2 = ', $num1 * $num2);
+// numArray.num1 = $('#num1').val();
+// numArray.num2 = $('#num2').val();
+////////////////////////////////////////////////// NOTE: Into Server ////////////////////////////////////////////////
+////////////////////////////////////////////////// NOTE: Into Server ////////////////////////////////////////////////
+////////////////////////////////////////////////// NOTE: Into Server ////////////////////////////////////////////////
 
-$.ajax({
-type: 'POST',
-url: '/',
-data: calaData,
-success: function(response) {
-  console.log('response from server is a success!');
-}
-});
-
-
-
+// console.log(calculation());
 }); // NOTE: FOR: $(document).ready(function () {
 console.log('Javascript sourced');
