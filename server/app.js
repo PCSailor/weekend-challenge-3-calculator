b1fc56a33feeadb859b2d5823b0282146e320b6a
Phil Curtis <Phil@PhilCurtis.io>
2017-02-27 21:07:45 -0600

    submission-02

--- server/app.js
+++ server/app.js
@@ -1,8 +1,4 @@
-// and the Server code would have something like
-// router.post('/division', function(req, res){
-//   //some code here
-// });
-// console.log('app.js success & mock server listening on Port 5000');
+console.log('mock server success & listening on Port 5000');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -11,7 +7,7 @@ app.use (bodyParser.urlencoded({extended: true})); // NOTE: does the request hav
 var calculations = [];
 var calcOne = "";
 var calcTwo = "";
-
+// NOTE: Receiving and Sending
 app.post('/', function(req, res) {
   var newCalc = req.body; // NOTE: .data from client.js becomes this req.body.  Now have the object from client.js within this variable
   console.log('client.js data is: ',newCalc);
@@ -21,27 +17,8 @@ app.post('/', function(req, res) {
   var calcOne = parseInt(newCalc.$num1);
   var calcTwo = parseInt(newCalc.$num2);
   var calcOps = newCalc.ops;
-  // console.log(calcOps);
-  // res.send('whaddup bitches');
-  console.log(calcOne, calcTwo);
-  console.log(typeof calcOne, calcTwo);
-
-
-  // if (calcOps == '+') {
-  //   console.log(calcOne + calcTwo);
-  //   res.send(calcOne + calcTwo)
-  // } else if (calcOps == '-') {
-  //   console.log(calcOne - calcTwo);
-  //   res.send(calcOne - calcTwo);
-  // } else if (calcOps == '*') {
-  //   console.log(calcOne * calcTwo);
-  //   res.send(calcOne * calcTwo);
-  // } else {
-  //   console.log(calcOne / calcTwo);
-  //   res.send(calcOne / calcTwo);
-  // }
-
-  if (calcOps == '+') {
+// NOTE: Math operations
+if (calcOps == '+') {
     var answer = (calcOne + calcTwo);
   } else if (calcOps == '-') {
     answer = (calcOne - calcTwo);
@@ -50,48 +27,11 @@ app.post('/', function(req, res) {
   } else {
     answer = (calcOne / calcTwo);
   }
-
+  // NOTE: Final Answer
 var objectToSend = {
   answer: answer
 }
 console.log('Final result = ',objectToSend);
-res.send(objectToSend);
-  // var stringResult = result.toString();
-  // console.log(result);
-  // console.log(typeof result);
-  // res.send(result.toString());
-  // console.log(result);
+res.send(objectToSend); // NOTE: use res.send (to send object/string/function back to client.js)
 }); // NOTE: app.post
-// console.log(calcOne, calcTwo);
-// app.get('/', function(req, res) {
-//   res.send(200);
-// });
-
-// NOTE: use res.send('whatever you're trying to send back');
-
-
-
-
-// NOTE: From Vanilla Node/app.js
-// var numberOne = 7;
-// var numberTwo = 4;
-// // NOTE: Multiplication
-// console.log(calculator.multiply(numberOne, numberTwo));
-// function multiply (firstNum, secondNum) {
-//   var product = firstNum * secondNum;
-//   return product;
-// }
-// // NOTE: Server
-// var crazy = calculator.crazy(numberOne);
-// var http = require('http'); // http built into node code as object
-//
-// http.createServer(function(req, res){
-//   res.writeHead(200);
-//   res.write('crazy'); // write status
-//   res.end();
-// }).listen(5000); //doesn't need to be 5000
-// NOTE: END From Vanilla Node/app.js
-
-
 app.listen(5000);
-// console.log('Listening to Port 5000');
--- server/public/client.js
+++ server/public/client.js
@@ -1,61 +1,29 @@
 $(document).ready(function () {
-console.log('jQuery sourced');
-// var num0 = '';
-// var num1 = '';
-// var operator = '';
-// var captureNum1 = false;
-
-// $('numArray').empty();  // NOTE: Empty the object
-
-// NOTE: set values to empty
-$('#clear').on('click', function() {
-  $('#num1').val('');
-  $('#num2').val('');
-  $('#calculation').val('');
-  console.log('Cleared!');
-});
-
-
-// NOTE: on click
-$('#result').on('click', function(){
-  console.log('Result Button clicked');
-  var $num1 = $('#num1').val();
-  var $num2 = $('#num2').val();
-  var ops = $('#operator').val();
-  var calcData = {$num1, $num2, ops};
-  // var objectToSend = {
-  //   num1: $num1,
-  //   num2: $num2,
-  //   operator: ops
-  // }
-  console.log(calcData);
-  // function ajaxPost() {
+  console.log('jQuery sourced');
+  // NOTE: set values to empty
+  $('#clear').on('click', function() {
+    $('#num1').val('');
+    $('#num2').val('');
+    $('#calculation').val('');
+    console.log('Cleared!');
+  });
+  // NOTE: on click
+  $('#result').on('click', function(){
+    var $num1 = $('#num1').val();
+    var $num2 = $('#num2').val();
+    var ops = $('#operator').val();
+    var calcData = {$num1, $num2, ops};
+    console.log(calcData);
+    // NOTE: FOR: function ajaxPost() {
     $.ajax({
       type: 'POST', // NOTE: These two lines are retrieving AND posting data
       url: '/',
-      data: calcData,
-      // data: objectToSend, // NOTE: data becomes req.body on the server side
+      data: calcData, // NOTE: data becomes req.body on the server side
       success: function(response) {
         console.log('Successful response is: ', response.answer);
         $('#calculation').val(response.answer)
-      }
-    });// NOTE: FOR: $.ajax({
-    // }
-  });// NOTE: FOR: $('#result').on('click', function(num1 , num2 ){
-
-      // $('#calculation').append('numbers-here');
-////////////////////////////////////////////////// NOTE: Failed Code ////////////////////////////////////////////////
-// console.log('num1 = ', $num1);
-// console.log('num2 = ', $num2);
-// var $result = $num1 * $num2; // Error:
-// console.log('$result = ', $result);
-// console.log('num1 * num2 = ', $num1 * $num2);
-// numArray.num1 = $('#num1').val();
-// numArray.num2 = $('#num2').val();
-////////////////////////////////////////////////// NOTE: Into Server ////////////////////////////////////////////////
-////////////////////////////////////////////////// NOTE: Into Server ////////////////////////////////////////////////
-////////////////////////////////////////////////// NOTE: Into Server ////////////////////////////////////////////////
-
-// console.log(calculation());
-}); // NOTE: FOR: $(document).ready(function () {
-console.log('Javascript sourced');
+      } // NOTE: FOR: success
+    }); // NOTE: FOR: $.ajax({
+    }); // NOTE: FOR: $('#result').on('click', function(num1 , num2 ){
+    }); // NOTE: FOR: $(document).ready(function () {
+      console.log('Javascript sourced');
